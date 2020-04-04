16d4896b145a096128a192f054391194d5fc1d48
Oleg Tomillo <oleg@Olegs-MacBook-Pro.local>
2020-04-03 20:43:33 -0700

    Updates

---
+++ precache-manifest.2df22bf46ff08d46fc8652c173a52173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e14a6d0cbf862dfdfa3ef2fff227541",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "06ab2521c01bd4e5e7ff",
+    "url": "/monsters-rolodex/static/css/main.bd299413.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/monsters-rolodex/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06ab2521c01bd4e5e7ff",
+    "url": "/monsters-rolodex/static/js/main.66343a3f.chunk.js"
+  },
+  {
+    "revision": "ac7692f361dea471e0d6",
+    "url": "/monsters-rolodex/static/js/runtime-main.ab61569e.js"
+  }
+]);
